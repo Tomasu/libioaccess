4348c3cb90b4351d7e0ad83c5466deb2de4be0be
Jonathan Seeley <sevalecan@gmail.com>
2015-02-28 22:13:52 -0500

    Made to compile in windows.

--- include/ioaccess/IOAccess.h
+++ include/ioaccess/IOAccess.h
@@ -9,8 +9,8 @@ namespace IOAccess
 {
 	enum FileMode
 	{
+		#if defined(__unix__)
 		ModeUnknown      = 0,
-		
 		ModeSetUid       = S_ISUID,
 		ModeSetGid       = S_ISGID,
 		ModeSticky       = S_ISVTX,
@@ -23,7 +23,7 @@ namespace IOAccess
 		ModeOtherRead    = S_IROTH,
 		ModeOtherWrite   = S_IWOTH,
 		ModeOtherExecute = S_IXOTH,
-		
+
 		ModeSocket  = S_IFSOCK, // socket
 		ModeLink    = S_IFLNK,  // symbolic link
 		ModeRegular = S_IFREG,  // regular file
@@ -31,58 +31,79 @@ namespace IOAccess
 		ModeDir     = S_IFDIR,  // directory
 		ModeChar    = S_IFCHR,  // character device
 		ModeFifo    = S_IFIFO   // FIFO
+
+		#elif defined(_WIN64) || defined(_WIN32)
+
+		ModeUnknown      = 0,
+		ModeOwnerRead    = S_IRUSR,
+		ModeOwnerWrite   = S_IWUSR,
+		ModeOwnerExecute = S_IXUSR,
+		ModeGroupRead    = S_IRGRP,
+		ModeGroupWrite   = S_IWGRP,
+		ModeGroupExecute = S_IXGRP,
+		ModeOtherRead    = S_IROTH,
+		ModeOtherWrite   = S_IWOTH,
+		ModeOtherExecute = S_IXOTH,
+
+		ModeRegular = S_IFREG,  // regular file
+		ModeBlock   = S_IFBLK,  // block device
+		ModeDir     = S_IFDIR,  // directory
+		ModeChar    = S_IFCHR,  // character device
+		ModeFifo    = S_IFIFO   // FIFO
+
+		#endif
 	};
-	
+
 	struct Timespec
 	{
 		int64_t sec;
 		int64_t nsec;
 	};
-	
+
 	struct StatInfo
 	{
 		uint64_t device;  /* ID of device containing file */
 		uint64_t inode;   /* inode number */
 		uint32_t mode;    /* protection */
 		int64_t nlink;    /* number of hard links */
-		
+
 		uint64_t uid;     /* user ID of owner */
 		uint64_t gid;     /* group ID of owner */
 		uint64_t rdev;    /* device ID (if special file) */
 		int64_t size;     /* total size, in bytes */
-		
+
 		Timespec atime;   /* time of last access */
 		Timespec mtime;   /* time of last modification */
 		Timespec ctime;   /* time of last status change */
-		
+
 		bool isDir() const { return mode & ModeDir; }
 		bool isFile() const { return mode & ModeRegular; }
 		bool isReadable() const { return mode & ModeOwnerRead || mode & ModeGroupRead || mode & ModeOtherRead; }
 		bool isWritable() const { return mode & ModeOwnerWrite || mode & ModeGroupWrite || mode & ModeOtherWrite; }
 		bool isExecutable() const { return mode & ModeOwnerExecute || mode & ModeGroupExecute || mode & ModeOtherExecute; }
 	};
-	
+
 	struct FileInterface
 	{
 		virtual bool open(const std::string &path, const std::string &mode) = 0;
-		
+
 		virtual size_t read(void *ptr, size_t len) = 0;
 		virtual size_t write(void *ptr, size_t len) = 0;
-		
+
 		virtual void close() = 0;
-		
+
 		virtual bool eof() = 0;
 		virtual bool error() = 0;
-		
+
 		virtual int32_t getErrno() = 0;
 	};
-	
+
 	class File : public FileInterface
-	{ 
+	{
 		public:
 			virtual ~File() { }
 	};
-	
+
 	struct DirectoryInterface
 	{
 		virtual bool open(const std::string &path) = 0;
@@ -90,13 +111,13 @@ namespace IOAccess
 		virtual void close() = 0;
 		virtual int32_t getErrno() = 0;
 	};
-	
-	class Directory : public DirectoryInterface 
-	{ 
+
+	class Directory : public DirectoryInterface
+	{
 		public:
 			virtual ~Directory() { }
 	};
-	
+
 	struct FileSystemInterface
 	{
 		virtual File *openFile(const std::string &path, const std::string &mode) = 0;
@@ -107,9 +128,9 @@ namespace IOAccess
 	};
 
 	void SetDefaultInteface(FileSystemInterface *fsi);
-	
+
 	bool Initialize(FileSystemInterface *default_fsi = nullptr);
-	
+
 	File *OpenFile(const std::string &path, const std::string &mode);
 	Directory *OpenDirectory(const std::string &path);
 	bool Stat(const std::string &path, StatInfo *si);
--- src/StdioInterface.cpp
+++ src/StdioInterface.cpp
@@ -19,10 +19,10 @@ namespace IOAccess
 			delete fh;
 			return nullptr;
 		}
-		
+
 		return fh;
 	}
-	
+
 	Directory *StdioInterface::openDir(const std::string &path)
 	{
 		StdioDirectory *fh = new StdioDirectory();
@@ -34,14 +34,14 @@ namespace IOAccess
 
 		return fh;
 	}
-	
+
 	bool StdioInterface::stat(const std::string &path, StatInfo *si)
 	{
 		struct stat sbuff;
 		int32_t ret = ::stat(path.c_str(), &sbuff);
 		if(checkError(ret != 0))
 			return false;
-		
+
 		si->device = sbuff.st_dev;
 		si->mode = sbuff.st_mode;
 		si->nlink = sbuff.st_nlink;
@@ -49,28 +49,38 @@ namespace IOAccess
 		si->gid = sbuff.st_gid;
 		si->rdev = sbuff.st_rdev;
 		si->size = sbuff.st_size;
-		
+		#if defined(__unix)
 		si->atime.sec = sbuff.st_atim.tv_sec;
 		si->atime.nsec = sbuff.st_atim.tv_nsec;
-		
+
 		si->ctime.sec = sbuff.st_ctim.tv_sec;
 		si->ctime.nsec = sbuff.st_ctim.tv_nsec;
-		
+
 		si->mtime.sec = sbuff.st_mtim.tv_sec;
 		si->mtime.nsec = sbuff.st_mtim.tv_nsec;
-		
+		#elif defined(_WIN64) || defined(_WIN32)
+		si->atime.sec = sbuff.st_atime;
+		si->atime.nsec = 0;
+
+		si->ctime.sec = sbuff.st_ctime;
+		si->ctime.nsec = 0;
+
+		si->mtime.sec = sbuff.st_mtime;
+		si->mtime.nsec = 0;
+		#endif
+
 		return true;
 	}
-	
+
 	bool StdioInterface::exists(const std::string &path)
 	{
 		struct stat sbuff;
 		int32_t ret = ::stat(path.c_str(), &sbuff);
-		
+
 		// if any error occurs, the file isn't accessible, so it might as well not exist.
 		if(checkError(ret != 0))
 			return false;
-		
+
 		return true;
 	}
 
@@ -78,7 +88,7 @@ namespace IOAccess
 	{
 		return errno;
 	}
-	
+
 	bool StdioInterface::checkError(bool err)
 	{
 		if(err)
@@ -86,19 +96,19 @@ namespace IOAccess
 			errno_ = errno;
 			return true;
 		}
-		
+
 		errno_ = 0;
 		return false;
 	}
-	
+
 	StdioFile::~StdioFile()
 	{
 		if(fh_)
 			close();
-		
+
 		errno_ = 0;
 	}
-	
+
 	bool StdioFile::checkError(bool err)
 	{
 		if(err)
@@ -106,61 +116,61 @@ namespace IOAccess
 			errno_ = errno;
 			return true;
 		}
-		
+
 		errno_ = 0;
 		return false;
 	}
-	
+
 	bool StdioFile::open(const std::string &path, const std::string &mode)
 	{
 		fh_ = fopen(path.c_str(), mode.c_str());
 		if(checkError(fh_ == nullptr))
 			return false;
-		
+
 		return true;
 	}
-	
+
 	size_t StdioFile::read(void *ptr, size_t len)
 	{
 		size_t ret = fread(ptr, 1, len, fh_);
 		checkError(ret != len);
-		
+
 		return ret;
 	}
-	
+
 	size_t StdioFile::write(void *ptr, size_t len)
 	{
 		size_t ret = fwrite(ptr, 1, len, fh_);
 		checkError(ret != len);
-		
+
 		return ret;
 	}
-	
+
 	void StdioFile::close()
 	{
 		int ret = fclose(fh_);
 		if(checkError(ret != 0))
 			return;
-		
+
 		fh_ = nullptr;
 		errno_ = 0;
 	}
-	
+
 	bool StdioFile::eof()
 	{
 		return feof(fh_) != 0;
 	}
-	
+
 	bool StdioFile::error()
 	{
 		return ferror(fh_) != 0;
 	}
-	
+
 	int32_t StdioFile::getErrno()
 	{
 		return errno_;
 	}
-	
+
 	bool StdioDirectory::checkError(bool err)
 	{
 		if(err)
@@ -168,25 +178,25 @@ namespace IOAccess
 			errno_ = errno;
 			return true;
 		}
-		
+
 		errno_ = 0;
 		return false;
 	}
-	
+
 	StdioDirectory::~StdioDirectory()
 	{
 		if(dir_)
 			close();
-		
+
 		errno_ = 0;
 	}
-	
+
 	bool StdioDirectory::open(const std::string &path)
 	{
 		dir_ = opendir(path.c_str());
 		if(checkError(dir_ == nullptr))
 			return false;
-		
+
 		path_ = path;
 
 		return true;
@@ -196,42 +206,42 @@ namespace IOAccess
 	{
 		std::string entname;
 		std::string path;
-		
+
 		do {
 			struct dirent *dent = readdir(dir_);
 			if(checkError(dent == nullptr))
 				break;
-			
+
 			// skip . and .. entries
 			if(strcmp(dent->d_name, ".") == 0 || strcmp(dent->d_name, "..") == 0)
 				continue;
-			
+
 			entname = dent->d_name;
-			
+
 			printf("StdioDirectory::read(fp:%i): %s\n", fullpath, entname.c_str());
 
 			if(fullpath)
 				ent->assign(path_ + std::string("/") + entname);
 			else
 				ent->assign(entname);
-		
+
 			return true;
-			
+
 		} while(1);
-		
+
 		return false;
 	}
-	
+
 	void StdioDirectory::close()
 	{
 		int32_t ret = closedir(dir_);
 		checkError(ret != 0);
 		dir_ = nullptr;
 	}
-	
+
 	int32_t StdioDirectory::getErrno()
 	{
 		return errno_;
 	}
-	
+
 }
